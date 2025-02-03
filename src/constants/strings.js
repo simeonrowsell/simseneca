d1ca753000b622916c49c3bbbf86b1674b2f07e5
simeonrowsell <sim@simseneca.design>
2025-02-03 16:18:10 +0000

    Home intro edit. Blog intro edit. Bigger fonts

--- src/constants/strings.js
+++ src/constants/strings.js
@@ -1,5 +1,5 @@
 export const GLOBAL_STRINGS = {  
-  BIO_BLURB: "Through design thinking and design doing, I discover and solve the most meaningful user problems whilst caring deeply about the real world impact of the experiences we build.",
+  BIO_BLURB: "Discovering and solving the most impactful user problems via design thinking and design doing. I care about the real world impact of the experiences we build.",
   SITE_TITLE: "Sim Seneca | Product designer, human being",
   SITE_DESC: "Sim Seneca\'s space on the web"
 };
